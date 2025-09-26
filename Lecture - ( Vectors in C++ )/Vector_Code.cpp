8912c6e8cfc2384583eafa4fe78bf761f1cbec63
CompiledByMayank <mayankpal8728@gmail.com>
2025-09-26 20:18:15 +0530

    Lecture - ( Vectors in C++ ) - Completed

--- Lecture - ( Vectors in C++ )/Vector_Code.cpp
+++ Lecture - ( Vectors in C++ )/Vector_Code.cpp
@@ -2,7 +2,41 @@
 
 //We need to include this header file because implementation of vector is already done in STL , so for that we need to use this header file
 #include<vector>
+#include<algorithm>
 using namespace std;
+
+int singleNumber(vector<int>& nums)
+{
+      int res = 0 ;
+    if(nums.size()==1)
+    {
+        return nums[0] ;
+    }
+    for(int i = 0 ; i < nums.size(); i++)
+    {
+       if(i==0)
+       {
+            if(nums[i]!=nums[i+1])
+            {
+                res = nums[i] ;
+                break;
+            }
+           
+       }
+       else
+       {
+           if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1])
+            {
+                res = nums[i] ;
+                break;
+            } 
+       }
+    }
+
+    return res;
+}
+
+
 int main()
 {
     //Declaration of vector
@@ -42,7 +76,17 @@ int main()
     vec.back();
 
     //To access element from specific position :- at()
-    vec.at(0);
+    // vec.at(0);
 
+    //Size of vector
+    cout << "Size of vector : " << secondvec.size() << endl;
 
+    //Capacity of Vector
+    cout << "Capacity of vector : " << secondvec.capacity() << endl;
+
+    //LC Question :- Single Number 
+    vector<int> nums = {8,3,1,1,3,3,8,0,0,12,12,11,11,818,818,10,10,772} ;
+    sort(nums.begin() , nums.end());
+    int Result = singleNumber(nums); 
+    cout << "Our Single Number is : " << Result << endl;
 }
